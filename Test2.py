ce1acf738f7748f8a7a099fc053bb598b426db11
Guadalupe Muñoz <52288369+alejandramuoz21@users.noreply.github.com>
2019-07-22 18:06:10 -0500

    Add files via upload

---
+++ Test2.py
@@ -0,0 +1,7 @@
+from Lineal import *
+from numpy import *
+a = array([[4, 2, 5], [2, 5, 8], [5, 4, 3]], float)
+b = array([18], [27.3], [16.2]], float)
+x = array([1.2], [2.1], [1.8])
+
+print(inversa(a,b))
